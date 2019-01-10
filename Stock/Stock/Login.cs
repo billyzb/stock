ad9b486c71051440cd9833eb0bc2527c82791c2c
billyzb <46557319+billyzb@users.noreply.github.com>
2019-01-10 05:04:40 -0500

    Added Stock Project Source Code

---
+++ Stock/Stock/Login.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stock
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox1.Focus();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+     
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //check login username & password
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=stock;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter(@"SELECT *  FROM [stock].[dbo].[Login] WHERE UseName='"+ textBox1.Text +"' and PassWord='" + textBox2.Text +"'",con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                this.Hide();
+                StockMain main = new StockMain();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("invalid usename & password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                button1_Click(sender, e);
+            }
+        }
+    }
+}
